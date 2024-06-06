8cb2e9bafed849e9125336880cd7966a77ec04ba
liangjiacheng <lainbo@foxmail.com>
2024-06-06 11:12:38 +0800

    feat: Add child element with radial gradient background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">
+  <div id="app" class="parent-element">
+    <div class="child-element">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -8,6 +10,17 @@
   min-height: 100vh;
   min-height: calc(var(--1dvh, 1vh) * 100);
   min-height: 100dvh;
-  background: #f8fafc;
+  background-color: white;
 }
+
+.child-element {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-image: radial-gradient(#e5e7eb 1px, transparent 1px);
+  background-size: 16px 16px;
+  -webkit-mask-image: radial-gradient(ellipse 50% 50% at 50% 50%, black 70%, transparent 100%);
+  mask-image: radial-gradient(ellipse 50% 50% at 50% 50%, black 70%, transparent 100%);
+}
+ 
 </style>
